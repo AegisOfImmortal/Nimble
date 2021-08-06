ff6356ee855546caf69d36534ccd438427fbbbe6
Tim Ding <ding@ucdavis.edu>
2021-08-06 09:48:51 -0700

    Update solutions

--- client.py
+++ client.py
@@ -1,13 +1,156 @@
-#!/usr/bin/env python3
+''' 2. Client python program '''
 
-import socket
+import cv2
+import numpy as np
+from multiprocessing import Process, Queue, Value
+import argparse
+import asyncio
+from aiortc import MediaStreamTrack, RTCPeerConnection, RTCSessionDescription, RTCIceCandidate
+from aiortc.contrib.signaling import BYE, TcpSocketSignaling, add_signaling_arguments
 
-HOST = '127.0.0.1'  # The server's hostname or IP address
-PORT = 65432        # The port used by the server
+class FrameTransport(MediaStreamTrack):
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.connect((HOST, PORT))
-    s.sendall(b'Hello, world')
-    data = s.recv(1024)
+    '''5. Transmit images (frames) using MediaStreamTrack'''
 
-print('Received', repr(data))
+    kind = "video"
+    def __init__(self, track):
+        super().__init__()
+        self.track = track
+
+    async def recv(self):
+
+        '''
+        Waits for the frame to receive
+            output: the frame received
+        '''
+
+        frame = await self.track.recv()
+        return frame
+
+async def consume_signaling(pc, signaling):
+    '''
+    Utility function to keep signal receive port and offer-answer active
+    Taken from aiortc client example
+    Also creates answer
+    '''
+    while True:
+        obj = await signaling.receive()
+
+        if isinstance(obj, RTCSessionDescription):
+            await pc.setRemoteDescription(obj)
+
+            if obj.type == "offer":
+                await pc.setLocalDescription(await pc.createAnswer())
+                await signaling.send(pc.localDescription)
+        elif isinstance(obj, RTCIceCandidate):
+            await pc.addIceCandidate(obj)
+        elif obj is BYE:
+            print("Exiting")
+            break
+
+def imageParse(q, X, Y):
+    '''
+    9. Parse the image and determine the current location of the ball
+    input:
+        q (Queue): multiprocessing queue for passing image data
+        X (Value): Shared variable for x coordinate
+        Y (Value): Shared variable for x coordinate
+    10. Store computed coordinates as values
+    '''
+    img = q.get()
+    gray_image = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    ret, thresh = cv2.threshold(gray_image, 127, 255, 0)
+    M = cv2.moments(thresh)
+    if (M["m00"] != 0):
+        X.value = int(M["m10"] / M["m00"])
+        Y.value = int(M["m01"] / M["m00"])
+
+async def analyzeTrack(pc, track):
+    '''
+    8. Send recieved image to process_a using queue
+    Poll server for frame, predict ball center via Process, send coordinates
+    over DataChannel.
+
+            Parameters:
+                    pc (RTCPeerConnection): Remote peer object
+                    track (RemoteStreamTrack): object to receive server frames
+
+    '''
+    localVideo = FrameTransport(track)
+    dc = pc.createDataChannel('coords')
+
+    X = Value('i', 0)
+    Y = Value('i', 0)
+    process_q = Queue()
+
+    '''
+    6. Display received images using opencv
+    7. Starts a new process_a
+    11. Send coodinates from the values of process_a
+    '''
+
+    while(True):
+        try:
+            process_a = Process(target = imageParse, args = (process_q, X, Y))
+            process_a.start()
+            frame = await localVideo.recv()
+
+            img = frame.to_ndarray(format="bgr24")
+            cv2.imshow("Client Feed", img)
+            cv2.waitKey(1)
+            process_q.put(img)
+            process_a.join()
+            dc.send("coords:" + str(X.value) + "," + str(Y.value))
+        except Exception as e:
+            print(e)
+
+
+
+async def main(pc, signaling):
+    '''
+    3. Using aiortc built-in TcpSocketSignaling
+    b. Receive aiortc offer and create answer
+    Input
+        pc: RTC remote Peer Connection
+        signaling : TcpSocketSignaling object
+
+    '''
+    params = await signaling.connect()
+
+    @pc.on("track")
+    async def on_track(track):
+        print("Track %s received" % track.kind)
+        await analyzeTrack(pc, track)
+
+    @pc.on("datachannel")
+    def on_datachannel(channel):
+        print(channel, "-", "created by remote party")
+
+        @channel.on("message")
+        def on_message(message):
+            print(channel, "<", message)
+
+    await consume_signaling(pc, signaling)
+
+
+if __name__ == "__main__":
+    print("Client started")
+    parser = argparse.ArgumentParser(description="Ball Position Detector - Client")
+
+    add_signaling_arguments(parser)
+    args = parser.parse_args()
+
+    signaling = TcpSocketSignaling(args.signaling_host, args.signaling_port)
+    pc = RTCPeerConnection()
+
+    loop = asyncio.get_event_loop()
+    try:
+        loop.run_until_complete(
+            main(pc, signaling)
+        )
+    except KeyboardInterrupt:
+        pass
+    finally:
+        loop.run_until_complete(signaling.close())
+        loop.run_until_complete(pc.close())
+        loop.run_until_complete(cv2.destroyAllWindows())
--- server.py
+++ server.py
@@ -1,18 +1,187 @@
-#!/usr/bin/env python3
+''' 1. Server python program '''
 
-import socket
+import cv2
+import numpy as np
+import argparse
+import asyncio
+from av import VideoFrame
+from aiortc import VideoStreamTrack, RTCPeerConnection, RTCSessionDescription, RTCIceCandidate
+from aiortc.contrib.signaling import BYE, TcpSocketSignaling, add_signaling_arguments
 
-HOST = '127.0.0.1'  # Standard loopback interface address (localhost)
-PORT = 65432        # Port to listen on (non-privileged ports are > 1023)
+class BouncingBall():
+    ''' 4. Generates 2D image ball bouncing '''
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.bind((HOST, PORT))
-    s.listen()
-    conn, addr = s.accept()
-    with conn:
-        print('Connected by', addr)
-        while True:
-            data = conn.recv(1024)
-            if not data:
-                break
-            conn.sendall(data)
+    def __init__(self):
+        ''' Initialization of Animation parameters '''
+
+        self.WIDTH = 720
+        self.HEIGHT = 480
+        self.dx = 10
+        self.dy = 10
+        self.xCo = 240
+        self.yCo = 240
+        self.ballRad = 10
+        self.ballColor = (0, 255, 0)
+        self.i = 0
+        self.predX = 0
+        self.predY = 0
+
+    def Inbound(self, maxBound, x):
+        '''
+        Inbound check for ball
+        Input:
+            maxBound: int for maximum coordinate
+            x: int for coordinate to be checked
+        Output:
+            Inbound check True/False
+            
+        '''
+        if (x >= maxBound or x <= 0):
+            return True
+        return False
+
+    def BouncingBall(self):
+        '''
+        Creation of one new frame of ball bouncing
+        Output;
+            img: img frame in numpy array
+            xCo,yCo; x,y coordinates
+
+        '''
+        img = np.zeros((self.HEIGHT, self.WIDTH, 3), dtype = 'uint8')
+        if self.Inbound(self.WIDTH, self.xCo):
+            self.dx *= -1
+        if self.Inbound(self.HEIGHT, self.yCo):
+            self.dy *= -1
+
+        self.xCo += self.dx
+        self.yCo += self.dy
+        self.i += 1
+        cv2.circle(img, (self.xCo, self.yCo), self.ballRad, self.ballColor, -1)
+        return (img, self.xCo, self.yCo)
+
+class FrameConstruct(VideoStreamTrack):
+    '''
+    5. Creates the bouncing ball and prepares for transfer to client
+    Use the VideoStreamTrack class to package the frames
+
+    '''
+
+    kind = "video"
+
+    def __init__(self, ball):
+        ''' Initialization of Ball '''
+
+        super().__init__()
+        self.ball =  ball
+        self.x = 0
+        self.y = 0
+
+
+    async def recv(self):
+        ''' Image is packed into a frame '''
+
+        pts, time_base = await self.next_timestamp()
+        img, self.x, self.y = self.ball.BouncingBall()
+        frame = VideoFrame.from_ndarray(img, format = "bgr24")
+        frame.pts = pts
+        frame.time_base = time_base
+        return frame
+
+
+async def consume_signaling(pc, signaling):
+    '''
+    Utility function to keep signal receive port and offer-answer active
+    Taken from aiortc server example
+    '''
+    while True:
+        obj = await signaling.receive()
+
+        if isinstance(obj, RTCSessionDescription):
+            await pc.setRemoteDescription(obj)
+        elif isinstance(obj, RTCIceCandidate):
+            await pc.addIceCandidate(obj)
+        elif obj is BYE:
+            print("Exiting")
+            break
+
+
+def computeErrors(x1, y1, x2, y2):
+    ''' Calculates the difference (error) within coordinaties '''
+
+    error = ((x1 - x2)**2 + (y1 - y2)**2)**(1/2)
+
+    print("Real Center: " + str(x1) + ", " + str(y1))
+    print("Pred Center: " + str(x2) + ", " + str(y2))
+    print("Error: " + str(error))
+
+
+async def main(pc, signaling):
+    '''
+    3. Using aiortc built-in TcpSocketSignaling
+    a. Create aiortc offer and send to client
+    Input
+        pc: RTC remote Peer Connection
+        signaling : TcpSocketSignaling object
+
+    '''
+    ball = BouncingBall()
+    track = FrameConstruct(ball)
+    params = await signaling.connect()
+
+    dc = pc.createDataChannel('chat')
+
+    @pc.on('icegatheringstatechange')
+    def pcIceGatherStateChange():
+        print('NEW Ice Gathering State %s' % pc.iceGatheringState)
+
+    @pc.on('iceconnectionstatechange')
+    def pcIceConnectionStateChange():
+        print('NEW Ice Connection State %s' % pc.iceConnectionState)
+
+    @pc.on('signalingstatechange')
+    def pcSignalingStateChange():
+        print('NEW Signaling State %s' % pc.signalingState)
+
+    @pc.on('track')
+    def pcTrack():
+        print('NEW track %s' % track.id)
+
+    @pc.on("datachannel")
+    def on_datachannel(channel):
+        @channel.on("message")
+        async def on_message(message):
+            '''12. Display received coordinates and compute error'''
+
+            if (message.startswith("coords")):
+                coords = message[7:].split(",")
+                computeErrors(ball.xCo, ball.yCo, int(coords[0]), int(coords[1]))
+
+    pc.addTrack(track)
+    await pc.setLocalDescription(await pc.createOffer())
+    await signaling.send(pc.localDescription)
+
+    await consume_signaling(pc, signaling)
+
+
+if __name__ == "__main__":
+    print("Server started")
+    parser = argparse.ArgumentParser(description="Ball Position Detector - Server")
+    add_signaling_arguments(parser)
+
+    args = parser.parse_args()
+
+    signaling = TcpSocketSignaling(args.signaling_host, args.signaling_port)
+    pc = RTCPeerConnection()
+
+
+    loop = asyncio.get_event_loop()
+    try:
+        loop.run_until_complete(
+            main(pc, signaling)
+        )
+    except KeyboardInterrupt:
+        pass
+    finally:
+        loop.run_until_complete(signaling.close())
+        loop.run_until_complete(pc.close())
